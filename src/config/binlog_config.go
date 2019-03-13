6db611c814d50693b8516b65e4da6e2cfee6a2de
aloxc <lisily101212>
2019-03-14 00:32:43 +0800

    重构目录结构

--- src/config/binlog_config.go
+++
@@ -1,9 +0,0 @@
-package config
-
-type BinlogServerConfig struct {
-	Addr string `json:"addr"`
-	// User is for MySQL user.
-	User string `json:"user"`
-	// Password is for MySQL password.
-	Password string `json:"password"`
-}
--- src/server/binlog_slave.go
+++
@@ -1,9 +0,0 @@
-package server
-
-import (
-	"sync/atomic"
-)
-type BinlogSlave struct {
-	started *atomic.Bool
-	
-}
